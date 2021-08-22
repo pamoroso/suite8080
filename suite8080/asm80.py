a8f15c91c7a47c05b7d067284f6803c22a8fe2b6
Paolo Amoroso <paolo.amoroso@gmail.com>
2021-08-22 13:44:54 +0000

    Set up assembler globals, create output writer, and flesh out assembly function

--- suite8080/asm80.py
+++ suite8080/asm80.py
@@ -4,11 +4,47 @@ import argparse
 from pathlib import Path
 
 
-def assemble(lines, outfile):
+# Current source line number
+lineno = 0
+
+# Address of current instruction
+address = 0
+
+# This is a 2-pass assembler, so keep track of which pass we're in
+source_pass = 1
+
+# Assembled machine code
+output = b''
+
+
+def parse():
+    """Parse an assembly source line."""
     pass
 
 
+def write_binary_file(filename):
+    """Write machine code output to a binary file."""
+    with open(filename, 'wb') as file:
+        file.write(output)
+
+
+def assemble(lines, outfile):
+    """Assemble source and write output to a file."""
+    global lineno, source_pass
+
+    source_pass = 1
+    for lineno, line in enumerate(lines):
+        parse()
+
+    source_pass = 2
+    for lineno, line in enumerate(lines):
+        parse()
+    
+    write_binary_file(outfile)
+
+
 def main(infile):
+    """Parse the command line and pass the input file to the assembler."""
     with open(infile, 'r') as file:
         lines = [line for line in file]
 
